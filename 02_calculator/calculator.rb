747cde1112ca9d39c7dfd1b8f58a250a2d58f705
andy-int <a@gura.com.ua>
2013-03-03 20:53:41 +0200

    add calculator.rb file

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,32 @@
+
+def add(a,b)
+  a+b
+end
+
+def subtract(a,b)
+  a-b
+end
+
+def sum(a)
+  sum = 0
+  a.each{|e| sum += e }
+  sum
+end
+
+def multiply(*a)
+  if a.length >= 1 then
+    res = 1
+    a.each{|e| res *= e}
+    res
+  end
+end
+
+def power(a,b)
+  a**b
+end
+
+def factorial(a)
+  f = 1
+  a.times{|x| f *= x+1}
+  f
+end
