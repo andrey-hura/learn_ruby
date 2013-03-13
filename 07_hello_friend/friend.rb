d225c8251fc8eb6b335c4573df9ebb2c8e1ac3b0
andy-int <a@gura.com.ua>
2013-03-13 09:39:25 +0200

    07 - "Update greeting method"

--- 07_hello_friend/friend.rb
+++ 07_hello_friend/friend.rb
@@ -1,7 +1,14 @@
 
 class Friend
-  def greeting(*a)
-    if a.size>0 then b=", #{a[0]}" end
-    "Hello#{b}!"
+
+#  def greeting(*a)
+#    if a.size>0 then b=", #{a[0]}" end
+#    "Hello#{b}!"
+#  end
+
+  def greeting(a='')
+    a = ", "+a if a !=''
+    "Hello#{a}!"
   end
+  
 end
