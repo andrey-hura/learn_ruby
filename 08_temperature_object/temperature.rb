17cddad9cf2b99516eedad1245b764a68fa8af53
andy-int <a@gura.com.ua>
2013-03-14 12:35:09 +0200

    08 - "Renamed variables"

--- 08_temperature_object/temperature.rb
+++ 08_temperature_object/temperature.rb
@@ -1,53 +1,46 @@
 
 class Temperature
 
-  def initialize(t={:c=>0})
-    if t.key?(:f) then
-      @f = t[:f]
-#      @c = (t[:f].abs-32)*5.0/9.0 # after refactoring
-      @c = self.ftoc(t[:f])
+  def initialize(temp={:c=>0})
+    if temp.key?(:f) then
+      @fah = temp[:f]
+      @cel = self.ftoc(temp[:f])
     else
-      @c = t[:c]
-#      @f = t[:c].abs*9.0/5.0+32 # after refactoring
-      @f = self.ctof(t[:c])
+      @cel = temp[:c]
+      @fah = self.ctof(temp[:c])
     end
   end
 
-  def ftoc(f) # after refactoring
-    (f.abs-32)*5.0/9.0
+  def ftoc(far)
+    (far-32)*5.0/9.0
   end
 
-  def ctof(c) # after refactoring
-    c.abs*9.0/5.0+32
+  def ctof(cel)
+    cel*9.0/5.0+32
   end
 
-  def in_fahrenheit
-    @f
+  def in_fahrenheit() @fah end
+
+  def in_celsius() @cel end
+
+  def self.from_celsius(temp)
+    self.new(:c => temp)
   end
 
-  def in_celsius
-    @c
-  end
-
-  def self.from_celsius(t)
-    self.new(:c => t)
-  end
-
-  def self.from_fahrenheit(t)
-    self.new(:f => t)
+  def self.from_fahrenheit(temp)
+    self.new(:f => temp)
   end
 
 end
 
 class Celsius < Temperature
-  def initialize(t)
-    super({:c => t})
+  def initialize(temp)
+    super({:c => temp})
   end
 end
 
 class Fahrenheit < Temperature
-  def initialize(t)
-    super({:f => t})
+  def initialize(temp)
+    super({:f => temp})
   end
 end
-
