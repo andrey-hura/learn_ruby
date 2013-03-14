250c9f092100134e2ae12c6e8069902f761bf669
andy-int <a@gura.com.ua>
2013-03-14 12:43:44 +0200

    09 - "Using attr_reader & renamed variables"

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -1,8 +1,10 @@
 
 class Book
 
- def title=(t)
-   @title = t
+ attr_reader :title
+
+ def title=(sentence)
+   @title = sentence
    @title.gsub!(/(\S{3,})/){ |x| x.capitalize }
    except = Regexp.union("And", "The", "Over", "Since",
                          "With", "About", "Under", "Behind",
@@ -14,10 +16,6 @@ class Book
 
    @title.gsub!(except){ |x| x.downcase }
    @title.gsub!(/(^\S)|(\bi\b)/){ |x| x.capitalize }
- end
-
- def title
-   @title
  end
 
 end
