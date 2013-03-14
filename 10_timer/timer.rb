04a701910e7912e79d94c87bd24e2589de6b575f
andy-int <a@gura.com.ua>
2013-03-14 12:49:46 +0200

    10 - "Using attr_accessor & renamed variables"

--- 10_timer/timer.rb
+++ 10_timer/timer.rb
@@ -1,21 +1,19 @@
 class Timer
+
+  attr_accessor :seconds
+
   def initialize
     @seconds = 0
   end
-  def seconds
-    @seconds
-  end
-  def seconds=(s)
-    @seconds = s
-  end
+
   def time_string
     s = @seconds
     "#{p((s/3600)%24)}:#{p((s/60)%60)}:#{p(s%60)}"
   end
-  
-  def padded(d)
-    d.to_s.size == 1 ? "0#{d}" : d
+
+  def padded(num)
+    num < 10 ? "0#{num}" : num
   end
-  
+
   alias p padded
 end
